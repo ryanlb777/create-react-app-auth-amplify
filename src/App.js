5eec7ba385025e75f5dbd4b6330304c0308a5811
ryanlb777 <ryan.lebon24@gmail.com>
2022-05-23 08:32:54 -0700

    Test this is a change

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1> Hello </h1>
         <AmplifySignOut />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            This is a change
           </p>
           <a
             className="App-link"
