bdb7ba29ee28999e8bd325c4c53cafc55bd78ba2
ryanlb777 <44009626+ryanlb777@users.noreply.github.com>
2023-01-16 04:23:21 +0000

    feat: new changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            This is a change
+            Another Change
           </p>
           <a
             className="App-link"
